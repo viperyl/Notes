cb85a7fb32df7b0225cea0a676c6034a5c1e96de
Viepr <shawnyangxu@gmail.com>
2022-02-08 13:35:58 +0000

    啊

---
+++ Leetcode/LC1.md
@@ -0,0 +1,24 @@
+
+
+### No.258
+
+Assume a number
+$$
+\Large a_0\times10^0 + ... + a_{n-1}\times10^{n-1}
+$$
+sum of every digits
+$$
+\Large a_0 + a_1 + ... + a_{n-1}
+$$
+mod 9 for original digits
+$$
+\begin{align}
+
+&(a_0\;mod\;9)(10^0\;mod\;9) + ... + (a_{n-1}\;mod\;9)(10^{n-1}\;mod\;9)\\
+&= (a_0\;mod\;9) + (a_1\;mod\;9) + ... + (a_{n-2}\;mod\;9) + (a_{n-1}\;mod\;9)\\
+&= (a_0 + a_1 + ... + a_{n-2} + a_{n-1})\;mod\;9
+
+\end{align}
+$$
+we build a connection between above original numbers and sum of its digits
+
--- Paper/FCN/Code/src/backbone.py
+++ Paper/FCN/Code/src/backbone.py
@@ -21,5 +21,86 @@ class Bottleneck(nn.Module):
 
         width = int(planes * (base_width/64.)) * groups
         self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU()
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+
+    class ResNet(nn.Module):
+        def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                     groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                     norm_layer=None):
+            super(ResNet, self).__init__()
+            if norm_layer is None:
+                norm_layer = nn.BatchNorm2d
+            self._norm_layer = norm_layer
+
+            self.inplanes = 64
+            self.dilation = 1
+
+            if replace_stride_with_dilation is None:
+                replace_stride_with_dilation = [False, False, False]
+
+            if len(replace_stride_with_dilation) != 3:
+                raise ValueError("replace_stride_with_dilation should be None or a 3-element tuple")
+
+            self.groups = groups
+            self.base_width = width_per_group
+            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
